acb53a5fbc2b3ad418a064b84a4f4314d9a0bd54
Hari Narayan <harinarayanmr@aol.com>
2026-08-07 01:40:24 +0530

    Add Lenis smooth scrolling and icon height fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import Lenis from 'lenis';
+	import 'lenis/dist/lenis.css';
 	import Nav from '$lib/components/Nav.svelte';
 	import '../app.css';
 
 	let { children } = $props();
+
+	onMount(() => {
+		const lenis = new Lenis({
+			autoRaf: true,
+			duration: 1.2,
+			easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+			smoothWheel: true,
+			wheelMultiplier: 1.0,
+			touchMultiplier: 1.5
+		});
+
+		return () => {
+			lenis.destroy();
+		};
+	});
 </script>
 
 <svelte:head>
