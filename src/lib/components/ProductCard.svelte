ac0075837e507869cbd58bc665700b30b206be41
Hari Narayan <harinarayanmr@aol.com>
2026-08-07 15:42:32 +0530

    Revert product title casing back to original all caps styling

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -13,7 +13,7 @@
 		<img src={product.image} alt={product.title} />
 	</div>
 	<div class="listing__title">
-		<h3>{product.title} <br /> <span>{product.subtitle}</span></h3>
+		<h3>{product.title} <br> <span>{product.subtitle}</span></h3>
 	</div>
 	<div class="listing__info">
 		<div class="listing__info-count">
@@ -40,7 +40,6 @@
 
 	.listing span {
 		font-weight: 500;
-		text-transform: none;
 	}
 
 	.listing__image {
