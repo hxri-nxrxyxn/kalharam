191d0dd430a932099c07e2c6248fcb89dd1ca2d8
Hari Narayan <harinarayanmr@aol.com>
2026-08-07 18:03:54 +0530

    Add social media SVGs (YouTube, Facebook, Instagram) in Footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,48 +3,72 @@
 </script>
 
 <footer>
-    <div class="footer__boxes">
-        <div class="footer__box">
-            <h3>Legal</h3>
-            <a href="#">Shipping Policy</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms of Service</a>
-            <a href="#">Return Policy</a>
-        </div>
-        <div class="footer__box">
-            <h3>Information</h3>
-            <a href="#">FAQ</a>
-            <a href="#">Blog</a>
-            <a href="#">Newsletter</a>
-            <a href="#">About Us</a>
-            <a href="#">Contact Us</a>
-        </div>
-    </div>
-    <div class="footer__media">
-        <div class="footer__media-icon">
-            <img src="" alt="youtube">
-            <a href="#">YouTube</a>
-        </div>
-        <div class="footer__media-icon">
-            <img src="" alt="Facebook">
-            <a href="#">Facebook</a>
-        </div>
-        <div class="footer__media-icon">
-            <img src="" alt="Instagram">
-            <a href="#">Instagram</a>
-        </div>
-    </div>
+	<div class="footer__boxes">
+		<div class="footer__box">
+			<h3>Legal</h3>
+			<a href="#shipping">Shipping Policy</a>
+			<a href="#privacy">Privacy Policy</a>
+			<a href="#terms">Terms of Service</a>
+			<a href="#return">Return Policy</a>
+		</div>
+		<div class="footer__box">
+			<h3>Information</h3>
+			<a href="#faq">FAQ</a>
+			<a href="#blog">Blog</a>
+			<a href="#newsletter">Newsletter</a>
+			<a href="#about">About Us</a>
+			<a href="#contact">Contact Us</a>
+		</div>
+	</div>
+	<div class="footer__media">
+		<div class="footer__media-icon">
+			<img src="/assets/filled-shapes/youtube.svg" alt="youtube" />
+			<a href="#youtube">YouTube</a>
+		</div>
+		<div class="footer__media-icon">
+			<img src="/assets/filled-shapes/facebook.svg" alt="facebook" />
+			<a href="#facebook">Facebook</a>
+		</div>
+		<div class="footer__media-icon">
+			<img src="/assets/filled-shapes/instagram.svg" alt="instagram" />
+			<a href="#instagram">Instagram</a>
+		</div>
+	</div>
 </footer>
 
 <style>
-    .footer__boxes {
-        padding-top: var(--spacing-xl);
-        display: flex;
-        background-color: var(--color-surface);
-    }
-    .footer__box {
-        display: flex;
-        flex-direction: column;
-        gap: var(--spacing-md);
-    }
+	footer {
+		display: flex;
+		padding: var(--spacing-xl);
+		background-color: var(--color-surface);
+		justify-content: space-between;
+	}
+	footer a {
+		color: var(--color-secondary);
+		font-weight: 500;
+	}
+	.footer__boxes {
+		display: flex;
+		gap: var(--spacing-xl);
+	}
+	.footer__media {
+		justify-content: flex-end;
+		align-items: flex-end;
+		display: flex;
+		gap: var(--spacing-lg);
+	}
+	.footer__media-icon {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--spacing-sm);
+	}
+	.footer__media-icon img {
+		height: var(--height-icon);
+		filter: var(--filter-secondary);
+	}
+	.footer__box {
+		display: flex;
+		gap: var(--spacing-md);
+	}
 </style>
